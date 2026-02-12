d79b2d70efb24001ab3a2953f6093b26a38ba789
Michael Gorman <michaelgorman@crc-dot1x-nat-10-239-228-99.bu.edu>
2026-02-11 22:58:44 -0500

    Fixed Assign02_03

--- assigns/02/Code/Assign02_03.java
+++ assigns/02/Code/Assign02_03.java
@@ -8,31 +8,38 @@ public class Assign02_03 {
 		int n = A.length;
         if (n < 3) return false;
 
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                int target = A[i] + A[j];
+        for (int k = 0; k < n; k++) {
+            int i = 0; // start pointer
+            int j = n - 1; // end pointer
 
-                // Look for k > j so i, j, k are automatically distinct
-                if (binarySearch(A, target, j + 1, n - 1) != -1) {
-                    return true;
+            // continue while the ponters don't cross
+            while (i < j) {
+                // make sure they distinct
+                if (i == k) { 
+                    i++; 
+                    continue; 
+                }
+                if (j == k) { 
+                    j--;
+                    continue;
+                }
+
+                int sum = A[i] + A[j];
+                int target = A[k];
+
+                if (sum == target) {
+                    return true; // found distinct i, j, k
+                } else if (sum < target) {
+                    i++;
+                } else {
+                    j--;
                 }
             }
         }
+        // nothing found
         return false;
     }
 
-    // manual binary search on A[lo..hi] returns index if found else -1
-    private static int binarySearch(Integer[] A, int key, int lo, int hi) {
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            int midVal = A[mid];
-
-            if (key < midVal) hi = mid - 1;
-            else if (key > midVal) lo = mid + 1;
-            else return mid;
-        }
-        return -1;
-    }
 
     public static void main(String[] argv) {
 	// Please write some code here for testing solve_3sum
@@ -40,12 +47,14 @@ public class Assign02_03 {
         Integer[] A2 = {1, 5, 10};        
         Integer[] A3 = {0, -1, 1, 2, 3};  
         Integer[] A4 = {2, 2, 4};         
-        Integer[] A5 = {7};               
+        Integer[] A5 = {7};
+        Integer[] A6 = {-3, -2, -1};               
 
         System.out.println(solve_3sum(A1)); // true
         System.out.println(solve_3sum(A2)); // false
         System.out.println(solve_3sum(A3)); // true
         System.out.println(solve_3sum(A4)); // true
         System.out.println(solve_3sum(A5)); // false
+        System.out.println(solve_3sum(A6)); // true
     }
 }
