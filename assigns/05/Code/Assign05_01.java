5ea53926a1826a24cc6389b1f5aac9af6c275184
Michael Gorman <michaelgorman@crc-dot1x-nat-10-239-54-226.bu.edu>
2026-03-04 17:47:26 -0500

    Implemented Assign05_01

---
+++ assigns/05/Code/Assign05_01.java
@@ -0,0 +1,121 @@
+import MyLibrary.FnList.*;
+    
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.BiFunction;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.ToIntBiFunction;
+
+public class Assign05_01 {
+
+    public static
+	<T extends Comparable<T>>
+	FnList<T> insertSort(FnList<T> xs) {
+	return insertSort(xs, (x1, x2) -> x1.compareTo(x2));
+    }
+//
+    public static<T> FnList<T>
+	insertSort(FnList<T> xs, ToIntBiFunction<T,T> cmp) {
+	// HX-2026-02-26: Please implement this method
+	// You can use while-loops but cannot make recursive
+	// calls.
+		// reverse the original list first
+		FnList<T> rev = new FnList<>();
+        FnList<T> cur = xs;
+
+        while (!cur.nilq()) {
+            rev = new FnList<>(cur.hd(), rev);
+            cur = cur.tl();
+        }
+
+		// stores the growing sorted list
+        FnList<T> sorted = new FnList<>();
+        cur = rev;
+
+		// insert each itme from rev into the sorted list
+        while (!cur.nilq()) {
+            T x0 = cur.hd();
+            cur = cur.tl();
+
+			// leftRev stores the part before the insertion point in reverse order
+            FnList<T> leftRev = new FnList<>();
+            FnList<T> scan = sorted;
+
+			// find where x0 should be inserted
+            while (!scan.nilq()) {
+                T hd = scan.hd();
+                int sgn = cmp.applyAsInt(x0, hd);
+
+				// insert before hd if x0 <= hd
+                if (sgn <= 0) {
+                    break;
+                }
+
+                leftRev = new FnList<>(hd, leftRev);
+                scan = scan.tl();
+            }
+
+			// insert x0 into correct position
+            FnList<T> merged = new FnList<>(x0, scan);
+
+			// put the left side back in front
+            while (!leftRev.nilq()) {
+                merged = new FnList<>(leftRev.hd(), merged);
+                leftRev = leftRev.tl();
+            }
+
+            sorted = merged;
+        }
+
+        return sorted;
+    }
+
+    
+
+    public static void main(String[] args) {
+	// Please write some testing code that applies
+	// insertSort to the following list of 1M numbers:
+	// 1, 0, 3, 2, 5, 4, 7, 6, 9, 8, 11, 10, ..., 999999, 999998.
+		final int N = 1_000_000;
+        FnList<Integer> xs = new FnList<>();
+
+		// build the list
+        for (int i = N - 2; i >= 0; i -= 2) {
+            xs = new FnList<>(i, xs);
+            xs = new FnList<>(i + 1, xs);
+        }
+
+		// sort list
+        FnList<Integer> sorted = insertSort(xs);
+
+		// check if the result is in right oreder
+        boolean ok = true;
+        FnList<Integer> cur = sorted;
+
+        if (!cur.nilq()) {
+            int prev = cur.hd();
+            cur = cur.tl();
+
+            while (!cur.nilq()) {
+                int now = cur.hd();
+                if (prev > now) {
+                    ok = false;
+                    break;
+                }
+                prev = now;
+                cur = cur.tl();
+            }
+        }
+
+        if (ok) {
+            System.out.println("Sorted correctly");
+        } else {
+            System.out.println("Not sorted correctly");
+        }
+    }
+    
+
+} // end of [public class Assign05_01{...}]
---
+++ libraries/MyLibrary/FnList/FnListSUtil.java
@@ -0,0 +1,144 @@
+package MyLibrary.FnList;
+
+import MyLibrary.FnList.*;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
+import java.util.function.ToIntBiFunction;
+
+public class FnListSUtil {
+//
+    public static<T>
+	FnList<T> nil() {
+	return new FnList<T>();
+    }
+    public static<T>
+	FnList<T>
+	cons(T x0, FnList<T> xs) {
+	return new FnList<T>(x0, xs);
+    }
+//
+    // HX: [length] is O(n)
+    public static<T>
+	int length(FnList<T> xs) {
+	int res = 0;
+	while (true) {
+	    if (xs.nilq()) break;
+	    res += 1; xs = xs.tl();
+	}
+	return res;
+    }
+//
+    public static<T>
+	FnList<T> reverse(FnList<T> xs) {
+	FnList<T> ys;
+	ys = nil();
+	while (!xs.nilq()) {
+	    ys = cons(xs.hd(), ys); xs = xs.tl();
+	}
+	return ys;
+    }    
+
+    public static<T>
+	FnList<T> append(FnList<T> xs, FnList<T> ys) {
+	if (xs.nilq()) {
+	    return ys;
+	} else {
+	    return cons(xs.hd(), append(xs.tl(), ys));
+	}
+    }
+
+    public static<T>
+	void foritm(FnList<T> xs, Consumer<? super T> work) {
+	while (xs.consq()) {
+	    work.accept(xs.hd()); xs = xs.tl();
+	}
+	return;
+    }
+
+    public static<T>
+	boolean forall(FnList<T> xs, Predicate<? super T> pred) {
+	while (true) {
+	    if (xs.nilq()) {
+		break;
+	    } else {
+		if (pred.test(xs.hd())) {
+		    xs = xs.tl(); continue;
+		} else {
+		    return false; // HX: counterexample found!
+		}
+	    }
+	}
+	return true;
+    }
+
+    public static<T>
+	void iforitm(FnList<T> xs, BiConsumer<Integer, ? super T> work) {
+	int i = 0;
+	while (xs.consq()) {
+	    work.accept(i, xs.hd()); i += 1; xs = xs.tl();
+	}
+	return;
+    }
+
+    public static<T>
+	boolean iforall(FnList<T> xs, BiPredicate<Integer, ? super T> pred) {
+	int i = 0;
+	while (true) {
+	    if (xs.nilq()) {
+		break;
+	    } else {
+		if (pred.test(i, xs.hd())) {
+		    i += 1; xs = xs.tl(); continue;
+		} else {
+		    return false; // HX: counterexample found!
+		}
+	    }
+	}
+	return true;
+    }
+
+    public static<T>
+	void System$out$print(FnList<T> xs) {
+    	System.out.print("FnList(");
+	iforitm(xs,
+          (i, itm) ->
+	  {
+	      if (i > 0) {
+		  System.out.print(",");
+	      }
+	      System.out.print(itm.toString());
+	  }
+	);
+	System.out.print(")");
+    }
+//
+    public static<T>
+	FnList<T>
+	insertSort(FnList<T> xs, ToIntBiFunction<T,T> cmp) {
+	if (nilq(xs)) {
+	    return xs;
+	} else {
+	    return
+	    insertSort_insert(insertSort(xs.tl(), cmp), xs.hd(), cmp);
+	}
+    }
+    private static<T>
+	FnList<T>
+	insertSort_insert(FnList<T> xs, T x0, ToIntBiFunction<T,T> cmp) {
+	if (nilq(xs)) {
+	    return cons(x0, xs);
+	} else {
+	    final T hd = xs.hd();
+	    final int sgn = cmp.applyAsInt(x0, hd);
+	    if (sgn <= 0) { // HX: for stableness
+		return cons(x0, xs); // [x0] is returned
+	    } else {
+		return cons(hd, insertSort_insert(xs.tl(), x0, cmp));
+	    }
+	}
+    }
+//
+} // end of [public class FnListSUtil{...}]
